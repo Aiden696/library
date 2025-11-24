e16fe20088fd967a6f79d93c55c4d4625b48951f
Aiden696 <musicjoy333@gmail.com>
2025-11-24 18:25:11 +0300

    добавлена форма добавления книги и проверка

--- scripts/db.js
+++ scripts/db.js
@@ -1,12 +1,25 @@
 async function addBooksFromJson() { //добавить книги из json в localStorage
     if (!localStorage.getItem('allBooks')) {
         try {
-            let response = await fetch ('books.json');
-            let data = await response.json()
-            localStorage.setItem('allBooks', JSON.stringify(data));
-        } catch {
-            console.log('error')
+            let response = await fetch('./books.json'); //запрашивает данные
+            let books = await response.json(); //парсит
+            let verifiedBooks = []; //массив проверенных книг
+            
+            books.forEach(book => {
+                if (addBook(book)) { //каждой книге присваевается функция addBook() для проверки
+                    verifiedBooks.push(book) //если книга прошла проверку она доб. в массив verifiedBooks
+                } else {
+                    console.log("В книге не хватает элемента");
+                }
+            });
+            localStorage.setItem('allBooks', JSON.stringify(verifiedBooks)); //преобразует массив в json и сохранияет его в localStorage
+            console.log(verifiedBooks.length)
+            return verifiedBooks;
+        } catch (error) {
+            console.error(error);
         }
+    } else {
+        console.log('Книги уже есть в localStorage');
     }
 }
 
@@ -24,6 +37,10 @@ export async function getAllBooks() { //взять книги из localStorage
     }
     return []
 }
+
+getAllBooks().then(books => { //then ждет пока книги загрузятся из getAllBooks()
+    console.log('Все книги из localStorage:', books);
+});
 
 async function addGenresFromJson() { //добавить книги из json в localStorage
     if (!localStorage.getItem('allGenres')) {
@@ -51,5 +68,29 @@ export async function getAllGenres() { //взять книги из localStorage
     }
     return []
 }
+
+function addBook(book) {
+    let elementsOfBooks = ['title', 'author', 'description', 'genre_id', 'cover'];
+    
+    for (let element of elementsOfBooks) {
+        if (!Object.keys(book).includes(element)) {
+            console.log(`Отсутствует: ${element}`)
+            return false;
+        }
+    };
+    
+    if (typeof book.title !== 'string') { //typeof оператор, который возвращает строку указывающую тип операнда
+        console.log('title должен быть строкой');
+        return false;
+    }
+        
+    if (book.title.length === 0) {
+        console.log('title не может быть пустым');
+        return false;
+    }
+    return true;
+}
+
+addBooksFromJson()
 
 //localStorage.clear()
--- scripts/index.js
+++ scripts/index.js
@@ -27,6 +27,9 @@ document.addEventListener('click', (e) => {
 
 let listOfGenres = document.getElementById('listOfGenres');
 let listOfAuthors = document.getElementById('listOfAuthors');
+let addBook = document.getElementById('addBook');
+let dialog = document.getElementById('modalDialog')
+let modalContent = document.getElementById('modalContent')
 
 listOfGenres.innerHTML = `<div class="showAllBooks">Показать все жанры</div>` + Object.values(genres).map(name => //список жанров в select
     `<div class="selectGenre">${name}</div>`
@@ -41,6 +44,104 @@ function hideCard(card) {
     card.classList.remove('visible');
     card.classList.add('hidden')
 }
+
+addBook.addEventListener('click', function() { // ф-ция добавления книги через форму
+    let section = document.createElement('section');
+    section.classList.add('bookDialog')
+    section.innerHTML = `
+            <form id="addBookForm" class="form">
+                <h2>Добавить книгу</h2>
+
+                 <div class="formSection">
+                    <label for="cover">Добавьте обложку для книги:</label>
+                    <label for="cover" class="customButtonForPicture"> Выберите файл </label>
+                    <span class="fileName" id="fileName">Файл не выбран</span>
+                    <input id="cover" type="file" name="cover" accept="image/*">
+                    <div id="imageForCover" class="imageForCover"></div>
+                </div>
+
+                <div class="formSection">
+                    <label for="title">Название:</label>
+                    <input id="title" type="text" name="title" required>
+                    <span id="titleError" class="errorMessage"></span>
+                </div>
+
+                <div class="formSection">
+                    <label for="author">Автор:</label>
+                    <input id="author" type="text" name="author" required>
+                    <span id="authorError" class="errorMessage"></span>
+                </div>
+
+                <div class="formSection">
+                    <label for="genre">Жанр:</label>
+                    <input id="genre" type="text" name="genre" required>
+                    <span id="genreError" class="errorMessage"></span>
+                </div>
+
+                <div class="formSection">
+                    <label for="description">Описание:</label>
+                    <input id="description" type="text" name="description" required>
+                    <span id="descriptionError" class="errorMessage"></span>
+                </div>
+
+                <button type="submit" class="submitBatton" id="buttonSend">Добавить книгу</button>
+                <div id="successMessage" class="successMessage"></div>
+            </form>
+    `
+    modalContent.append(section)
+    dialog.showModal()
+    showImageForBook()
+})
+
+function showImageForBook() {
+    let coverInput = document.getElementById('cover');
+    let imageForCover = document.getElementById('imageForCover');
+
+    coverInput.addEventListener('change', function(e) {
+        let file = e.target.files[0]; //первый выбранный файл из массива файлов
+        if (file) {
+            let reader = new FileReader(); //встроенный js объект для чтения файлов из браузера
+            
+            reader.onload = function(e) { //когда файл загружен
+                imageForCover.innerHTML = `<img src="${e.target.result}" class="coverImage"> `;
+            };
+            
+            reader.readAsDataURL(file); //метод, который читает файл и преобразует его в Data URL строку (специальный формат, который позволяет встраивать файлы прямо в HTML/CSS/JS код)
+        }
+    });
+
+    coverInput.addEventListener('change', function(e) { // для отображения выбранной обложки
+    let fileName = document.getElementById('fileName');
+    if (this.files[0]) {
+        fileName.textContent = this.files[0].name;
+    } else {
+        fileName.textContent = 'Файл не выбран';
+    }
+});
+}
+
+document.addEventListener('click', function(e) {
+    if (e.target.classList.contains('closeButton')) {
+        closeDialog();
+    }
+});
+
+// Закрытие по ESC и клику вне окна
+dialog.addEventListener('click', function(e) {
+    if (e.target === dialog) {
+        closeDialog();
+    }
+});
+
+
+function closeDialog() {
+  let dialog = document.getElementById('modalDialog');
+  if (dialog) {
+    dialog.close();
+    modalContent.innerHTML = '';
+  }
+}
+
 
 async function displayBooks() {
     let books = await getAllBooks();
